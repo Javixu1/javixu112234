f662d5967c69331e473eb8fe2b04c41209133cf6
Javier <Javier@Javier-HP>
2016-11-25 12:58:43 +0100

    ...

--- PruebaJavaFX_Javier_DeAndres/src/application/Main.java
+++ PruebaJavaFX_Javier_DeAndres/src/application/Main.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+/**
+ * 
+ * @author Javier
+ *
+ */
 
 public class Main extends Application {
 	@Override
